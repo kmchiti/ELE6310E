58eb79c57d46986750c8f3f511d6b41c42b527e1
François Leduc-Primeau <francoislp@gmail.com>
2023-02-19 13:25:42 -0500

    removed the need to hardcode the location of the timeloop install script within the script

--- A2/install_timeloop/install_timeloop.sh
+++ A2/install_timeloop/install_timeloop.sh
@@ -1,26 +1,35 @@
-# Installation script for Timeloop-Accelergy on Google Colab
+# Installation script for Timeloop-Accelergy on Ubuntu and Google Colab
 # Author: Francois Leduc-Primeau <francois.leduc-primeau@polymtl.ca>
 
-# *** Some steps are needed before running the script ***
+# Usage: source <SCRIPT LOCATION>/install_timeloop.sh
+# where <SCRIPT LOCATION> can be "." if your current
+# directory is the location of this script.
 
-# TODO: mount google drive before sourcing this script, for instance using the
-# following notebook cell:
+# NOTE: On Google Colab, you probably want to access this script from
+# a location in your google drive. Your google drive can be mounted by
+# executing the following notebook cell:
 ### from google.colab import drive
 ### drive.mount('/content/drive')
 
-# TODO: Adjust the first path below to the location of the scripts
-ln -s /content/drive/MyDrive/ELE6310E/ELE6310E-public-repo/A2/install_timeloop ~/install_tl
+# ---------------------------------------------------------------------
 
-# Then you can "source <SCRIPT LOCATION>/install_timeloop.sh" to run
-# this script, where <SCRIPT LOCATION> can be "." if your current
-# directory is the location of this script.
+# Get location of this script
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+# Create a symlink in the home directory
+ln -s ${DIR} ~/install_tl
 
-# ------- You should not need to modify anything below this line ---------
 cd ~
 source ~/install_tl/install_tl_step0.sh
 
 source ~/install_tl/install_tl_step1.sh
-# should now be in a python virtual environment
+# we should now be in a python virtual environment
 
 source ~/install_tl/install_tl_step2.sh
 
